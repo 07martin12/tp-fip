53a9422eedd8c3bbdfb99924fcfa8c1d602f6558
Martín <136357272+07martin12@users.noreply.github.com>
2024-10-01 13:56:07 -0300

    Update productoEscalar.ts

--- clase-7/productoEscalar.ts
+++ clase-7/productoEscalar.ts
@@ -1,10 +1,9 @@
 import* as rls form 'readline-sync'; 
 
-const maxArr_1: number = rls.question ("ingrese la longitud que tendra el arreglo numero 1");
-const maxArr_2: number = rls.question ("ingrese la longitud que tendra el arreglo numero 2");
+const maxArr: number = rls.question ("ingrese la longitud que tendran los arreglos 1 y 2");
 
-let arr_1: number [] = cargarArr(maxArr_1);
-let arr_2: number [] = cargarArr(maxArr_2);
+let arr_1: number [] = cargarArr(maxArr);
+let arr_2: number [] = cargarArr(maxArr);
 
 console.log ("el producto escalar para los arreglos\narr_1: " + publicarArr (arr_1) + "\narr_2: " + publicarArr (arr_2) + "\nes de: " + productoEscalar (arr_1,arr_2)); 
 
@@ -25,16 +24,19 @@ function publicarArr (arr:number []) {
 }
 
 function productoEscalar (arr_1: number [], arr_2: number []) {
-  let productoArr_1: number = calcularProducto (arr_1);
-  let productoArr_2: number = calcularProducto (arr_2);
+  //ambos arreglos poseen la misma longitud
+  let producto: number = 1;
+  let suma: number = 0; 
 
-  return (productoArr_1 + productoArr_2);
+  for (let pos: number = 0; pos < arr.length; pos++) {
+      producto = arr_1 [pos] * arr_2 [pos]; 
+      suma += producto; 
+  }
+  
+  return suma;
 }
 
 function calcularProducto (arr: number []) {
-  let producto: number = 1;
-  
-  for (let pos: number = 0; pos < arr.length; pos++) {
       producto *= arr [pos]; 
   }
    return producto; 
